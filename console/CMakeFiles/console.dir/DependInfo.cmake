1be8d3f8f6bff684c4bf06efd0685b0284efdcd4
Atifa <a_atifa@mail.ru>
2020-05-19 03:10:05 +0400

    sdl with cmake is added ,problem in cmake

---
+++ console/CMakeFiles/console.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/atifa/Desktop/GameOfLife/console/print_with_ansi.c" "/home/atifa/Desktop/GameOfLife/console/CMakeFiles/console.dir/print_with_ansi.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "USING_LIB="
+  "console_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "console"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
