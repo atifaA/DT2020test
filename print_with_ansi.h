45e320887b4809958ec0f96655de6de50f5d7a68
Amirlin <48647794+Amirlin@users.noreply.github.com>
2020-05-18 07:22:26 +0400

    Delete print_with_ansi.h

--- print_with_ansi.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef PRINT_TO_CONSOLE_H
-#define PRINT_TO_CONSOLE_H
-    #define TRUE 1
-    #define FALSE 0
-    #include "game.h"
-    void BoardPrinting_ansi(struct board Board, unsigned short FALSEorTRUE);
-#endif
