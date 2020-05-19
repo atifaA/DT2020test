3ba3bace6906b0d50e3aa04cfb8d51bd1084964c
Atifa <a_atifa@mail.ru>
2020-05-19 16:52:41 +0400

    modificationa on initializing the board size

--- main.c
+++ main.c
@@ -4,21 +4,41 @@
 #include "console/print_with_ansi.h"
 #include "printing_SDL2/print_with_sdl.h"
 #include <getopt.h>
+#include <signal.h>
+
+
+int quit = 0;
+
+void quit_signal(){
+    quit = 1;
+}
+
+
 int main(int argc, char const *argv[])
 {
     struct board Board;
-    create_random_board(&Board, CIRCULAR_BOARD);
+    user_init_param(&Board, 500, 50, CIRCULAR_BOARD);
+    create_random_board(&Board);
     BoardPrinting_ansi(Board, FALSE);
     // print_SDL_print(Board);
+    //SDL_Event e;
+    signal(SIGINT, quit_signal);
 
-
-    while(1){
+    while(!quit){
         sleep(1);
         Board = create_new_board(Board);
-       BoardPrinting_ansi(Board, FALSE);
+        BoardPrinting_ansi(Board, FALSE);
         // print_SDL_print(Board);
+        // while (SDL_PollEvent(&e)){
+            // if(e.type == SDL_QUIT){
+            //     quit = 1;
+            // }
+        //}
 
     }
+
+    //print_SDL_close();
+    destroy_board(&Board);
 
     return 0;
 }
