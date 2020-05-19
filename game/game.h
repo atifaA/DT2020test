6fd53e642c52f27d5bdfb052aa2079088e5a7bfc
Atifa <a_atifa@mail.ru>
2020-05-19 10:27:50 +0400

    initializing board size is implemented

--- game/game.h
+++ game/game.h
@@ -2,8 +2,9 @@
 #ifndef GAME_H
 #define GAME_H
 
-    #define HEIGHT_OF_BOARD 40
-    #define WIDTH_OF_BOARD 40
+    // #define HEIGHT_OF_BOARD 40
+    // #define WIDTH_OF_BOARD 40
+
     #define DEAD_CELL 0
     #define ALIVE_CELL 1
     #define CIRCULAR_BOARD 1
@@ -12,15 +13,21 @@
 
 
     struct board{
-        unsigned short cells[WIDTH_OF_BOARD][HEIGHT_OF_BOARD];
-        unsigned short circular_flag;
+        // unsigned short cells[WIDTH_OF_BOARD][HEIGHT_OF_BOARD];
+        // unsigned short circular_flag;
+        unsigned short **cells; 
+        unsigned short type_flag;
+        int height;
+        int width; 
+
     };
 
 //new random
-    void create_random_board(struct board *Board, unsigned short circular_flag);
+    void create_random_board(struct board *Board);
 
     int clipped_neighbours(struct board Board, int x, int y);
     int circular_neighbour(struct board Board, int x, int y);
     struct board create_new_board(struct board Board);  //next board
-
+    void user_init_param(struct board *Board, int width, int height, unsigned short type_flag);
+    void destroy_board(struct board *Board);
 #endif
